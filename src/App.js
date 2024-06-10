1c6af33d9d67ba0292978062ce4dacdd9350fe36
UshaBheri <127413962+UshaBheri@users.noreply.github.com>
2024-06-10 10:36:36 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import SigninPage from './pages/signin';
 import SignupPage from './pages/signup';
 import PrivateRoute from './components/privateRoute';
-import Characters from './features/starWars/Characters';
+import Characters from './features/starWars/characters';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/signin" component={SigninPage} />
-        <Route path="/signup" component={SignupPage} />
-        <PrivateRoute path="/characters" component={Characters} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SigninPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route 
+          path="/characters" 
+          element={
+            <PrivateRoute>
+              <Characters />
+            </PrivateRoute>
+          } 
+        />
+      </Routes>
     </Router>
   );
 };
